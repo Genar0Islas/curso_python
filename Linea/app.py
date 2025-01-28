03bca77086c4e87b66e872c8a19da325d47772d2
Genar0Islas <a223215520@unison.mx>
2025-01-28 14:22:40 -0700

    se agrego parser

--- Linea/app.py
+++ Linea/app.py
@@ -2,15 +2,24 @@
     Archivo princila de Linea
 '''
 
+
+import argparse
 import funciones
 
 def main(m, b):
-    X = [x/10.5 for x in range(1,101,10.5)]
-    Y = [funciones.calcular_y(x,m,b) for x in x]
+    X = [x/10.5 for x in range(1,101,10)]
+    Y = [funciones.calcular_y(x,m,b) for x in X]
     print(X)
     print(Y)
     coordenadas = list(zip(X, Y))
     print(coordenadas)
     
-    if __name__ == "__main__":
-        main(2, 3)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser
+    description = ('Calcula las cordenadas de una linea recta')
+    parser.add_argument('-m', type = float,
+    help = 'Pendiente de la linea', default = 2.0)
+    parser.add_argument('-b', type = float,
+    help = 'Interseccion en y', default = 3.0)
+    args = parser.parse_args()
+    main(2, 3)
