790b2616d2d9da32755bf710e97c3b303707db29
Genar0Islas <a223215520@unison.mx>
2025-01-30 14:22:55 -0700

    IA

--- Gato/Tablero.py
+++ Gato/Tablero.py
@@ -1,6 +1,7 @@
 '''
 Tablero.py dibuja el tablero del juego del gato
 '''
+import random
 def dibuja_tablero(simbolos:dict):
     print(f'''
     {simbolos['1']} | {simbolos['2']} | {simbolos['3']}
@@ -10,6 +11,33 @@ def dibuja_tablero(simbolos:dict):
     {simbolos['7']} | {simbolos['8']} | {simbolos['9']}
     ''')    
 
+def ia(simbolos:dict):
+    '''
+    Estrategia de la computadora
+    '''
+    ocupado = True
+    while ocupado is True:
+        x = random.choice(numeros)
+        if simbolos[x] not in ['X','O']:
+            simbolos[x] = 'O'
+            ocupado = False
+
+    
 if __name__ == '__main__':
     numeros = [str(i) for i in range(1,10)]
     dsimbolos = {x:x for x in numeros}
+    dibuja_tablero(dsimbolos)
+    ia(dsimbolos)
+    dibuja_tablero(dsimbolos)
+    
+    '''
+    x = random.choice(numeros)
+    numeros.remove(x)
+    dsimbolos[x] = 'X'
+    dibuja_tablero(dsimbolos)
+
+    o = random.choice(numeros)
+    numeros.remove(o)
+    dsimbolos[o] = 'O'
+    dibuja_tablero(dsimbolos) 
+    '''
